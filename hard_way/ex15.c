b433ca9672c0bc25561dc39163874cb73e54777b
whg <wanghg@skylinematrix.com>
2023-09-08 15:45:41 +0800

    ex15

--- hard_way/ex15.c
+++ hard_way/ex15.c
@@ -5,7 +5,7 @@ int main() {
     char *names[] = {"Alan", "Frank", "Mary", "John", "Lisa"};
 
     int count = sizeof(ages) / sizeof(int);
-    int i = 0;
+    int i;
 
     for(i=0;i<count;i++){
         printf("%s has %d years alive.\n", names[i], ages[i]);
@@ -16,17 +16,20 @@ int main() {
     char **cur_name = names;
 
     for(i=0;i<count;i++){
+        // 解引用（指针 + 偏移量）
         printf("%s is %d years old.\n", *(cur_name+i), *(cur_age+i));
     }
     printf("--------------------\n");
 
     for(i=0;i<count;i++){
+        // 指针使用数组方式访问 = 指针 + 偏移量
         printf("%s is %d years old again.\n", cur_name[i], cur_age[i]);
     }
     printf("--------------------\n");
 
-    for(cur_name=names,cur_age=ages;(cur_age-ages)<count;cur_name++,cur_age++){
-        printf("%p - %p = %ld\n", cur_age, ages, (cur_age-ages));
+    long diff;
+    for(cur_name=names,cur_age=ages;(diff=cur_age-ages)<count;cur_name++,cur_age++){
+        printf("%p - %p = %ld\n", cur_age, ages, diff);
         printf("%s lived %d years so far.\n", *cur_name, *cur_age);
     }
     printf("--------------------\n");
