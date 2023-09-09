0fc8c4456deac44603bdd0c1e9232a007c031aaa
whg <wanghg@skylinematrix.com>
2023-09-10 01:23:47 +0800

    ex16

---
+++ hard_way/ex16.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Person{
+    char* name;
+    int age;
+} S_Person;
+
+S_Person* Person_create(char* name, int age){
+    S_Person* person = malloc(sizeof(S_Person)) ;
+    assert(person != NULL);
+    person->name = strdup(name);
+    person->age = age;
+    return person;
+}
+
+void Person_destroy(S_Person* person){
+    assert(person != NULL);
+    free(person->name);
+    free(person);
+}
+
+void Person_print(S_Person* person){
+    printf("pointer:%p, name:%s, age:%d\n", person, person->name, person->age);
+}
+
+int main() {
+    S_Person whg;
+    whg.name = "whg";
+    whg.age = 37;
+    Person_print(&whg);
+
+    S_Person* joe = Person_create("Joe", 32);
+    Person_print(joe);
+    S_Person* frank = Person_create("Frank", 20);
+    Person_print(frank);
+
+    joe->age += 20;
+    Person_print(joe);
+    frank->age += 20;
+    Person_print(frank);
+
+    Person_destroy(joe);
+    Person_destroy(frank);
+    return 0;
+}
--- hard_way/test_2.c
+++ hard_way/test_2.c
@@ -9,6 +9,26 @@ int main() {
     // printf("a=%d, *a_p=%d\n",a, *a_p);
     *a_p = 11;
     // printf("a=%d, *a_p=%d\n",a, *a_p);
+    int** a_pp;
+    a_pp = &a_p;
+    // printf("a=%d, *a_p=%d, **a_pp=%d\n",a, *a_p, **a_pp);
+    **a_pp = 12;
+    // printf("a=%d, *a_p=%d, **a_pp=%d\n",a, *a_p, **a_pp);
+
+    /*
+    char *s[] = {"Ali", "Bob"};
+    char** s_p;
+    printf("s address=%p, s_p address=%p, s_p=%p\n",&s, &s_p, s_p);
+    s_p = s;
+    printf("s address=%p, s_p address=%p, s_p=%p\n",&s, &s_p, s_p);
+    printf("s[0]=%s, s[1=]%s, *s_p=%s\n",s[0], s[1], *s_p);
+    *s_p = "whg";
+    printf("s[0]=%s, s[1=]%s, *s_p=%s\n",s[0], s[1], *s_p);
+    s_p++;
+    *s_p = "amy";
+    printf("s address=%p, s_p address=%p, s_p=%p\n",&s, &s_p, s_p);
+    printf("s[0]=%s, s[1=]%s, *s_p=%s\n",s[0], s[1], *s_p);
+    */
     return 0;
 }
 
@@ -26,6 +46,53 @@ main:
 
         movq    -8(%rbp), %rax      // 下面使用括号解引用，即把11赋值给rax所指向的地址
         movl    $11, (%rax)         // *a_p = 11;
+
+        movl    $0, %eax
+        popq    %rbp
+        ret
+*/
+
+/**
+main:
+        pushq   %rbp
+        movq    %rsp, %rbp
+
+        movl    $13, -12(%rbp)
+
+        leaq    -12(%rbp), %rax
+        movq    %rax, -24(%rbp)
+
+        movq    -24(%rbp), %rax
+        movl    $11, (%rax)
+
+        leaq    -24(%rbp), %rax     // 先用lea取得变量a_p的地址，赋值给rax
+        movq    %rax, -8(%rbp)      // a_pp = &a_p;
+
+        movq    -8(%rbp), %rax      // 下面使用2次括号进行2次解引用，即把12赋值给rax所指向的地址
+        movq    (%rax), %rax        // 第1次解引用
+        movl    $12, (%rax)         // 第2次解引用，**a_pp = 12;
+
+        movl    $0, %eax
+        popq    %rbp
+        ret
+ */
+
+/**
+.LC0:
+        .string "Alan"
+.LC1:
+        .string "whg"
+main:
+        pushq   %rbp
+        movq    %rsp, %rbp
+
+        movq    $.LC0, -16(%rbp)
+
+        leaq    -16(%rbp), %rax
+        movq    %rax, -8(%rbp)
+
+        movq    -8(%rbp), %rax
+        movq    $.LC1, (%rax)
 
         movl    $0, %eax
         popq    %rbp
