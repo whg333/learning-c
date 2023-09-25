d46fabdbbd154011052b7c4b7bf53dc0bbb3372a
whg <wanghg@skylinematrix.com>
2023-09-26 02:01:57 +0800

    ex28 whg

---
+++ hard_way/c-skeleton/src/whg.cpp
@@ -0,0 +1,9 @@
+//
+// Created by whg on 2023/9/26.
+//
+
+#include "whg.h"
+
+int add(int a, int b){
+    return a + b;
+}
---
+++ hard_way/c-skeleton/src/whg.h
@@ -0,0 +1,10 @@
+//
+// Created by whg on 2023/9/26.
+//
+
+#ifndef WHG_C_WHG_H
+#define WHG_C_WHG_H
+
+int add(int a, int b);
+
+#endif //WHG_C_WHG_H
