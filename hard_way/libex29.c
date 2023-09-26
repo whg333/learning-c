0c8dae89adddbaba6b38b3b62b30bc85f5577311
whg <wanghg@skylinematrix.com>
2023-09-27 01:36:47 +0800

    ex29

--- hard_way/libex29.c
+++ hard_way/libex29.c
@@ -2,6 +2,12 @@
 // Created by whg on 2023/9/26.
 //
 
+/**
+ * gcc -std=c99 -fPIC -c libex29.c -o libex29.o
+ * gcc -shared -o libex29.so libex29.o
+ * gcc -Wall -g -std=c99 ex29.c -ldl -o ex29
+ */
+
 #include <ctype.h>
 #include "dbg.h"
 
